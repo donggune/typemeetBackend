d1bb56cb8307359f3244298ee839e294e928b45d
admin lion <admin@admin.com>
2023-04-15 19:34:26 +0900

    first commit

---
+++ typemeet/src/main/java/com/mysite/typemeet/TypemeetApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.typemeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypemeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypemeetApplication.class, args);
+	}
+
+}
---
+++ typemeet/src/test/java/com/mysite/typemeet/TypemeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.typemeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypemeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
